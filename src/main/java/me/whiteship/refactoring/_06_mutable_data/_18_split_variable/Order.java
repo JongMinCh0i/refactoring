3feadae070688ab3a5b587c404a10caaef90c188
JongMinCh0i <wonderland314592@gmail.com>
2023-10-15 23:44:00 +0900

    6.1 리팩토링 18. 변수 쪼개기

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Order.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Order.java
@@ -1,10 +1,14 @@
 package me.whiteship.refactoring._06_mutable_data._18_split_variable;
 
+//inputValue 을 그대로 쓰는 것 보다 명확한 코드가 될 수 있음
 public class Order {
 
     public double discount(double inputValue, int quantity) {
-        if (inputValue > 50) inputValue = inputValue - 2;
-        if (quantity > 100) inputValue = inputValue - 1;
-        return inputValue;
+        double result = inputValue;
+
+        if (inputValue > 50) result -= 2;
+        if (quantity > 100) result -= 1;
+
+        return result;
     }
 }
