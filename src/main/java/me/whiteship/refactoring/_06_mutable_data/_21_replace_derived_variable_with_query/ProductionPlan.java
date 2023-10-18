5cdff951f447746eacba347f079757d9322af86b
JongMinCh0i <wonderland314592@gmail.com>
2023-10-18 22:01:07 +0900

    6.4 리팩토링 21. 파생 변수를 질의 함수로 바꾸기

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/ProductionPlan.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/ProductionPlan.java
@@ -5,15 +5,17 @@ import java.util.List;
 
 public class ProductionPlan {
 
-    private double production;
     private List<Double> adjustments = new ArrayList<>();
+//    : 계산을 통해서 얻게 되는 값은 변수가 아닌 계산 식으로 두는것이 좋다.
+//       그리고 그 계산 식은 함수로 제공되는것이 제일 좋다.
+//    private double production;
 
     public void applyAdjustment(double adjustment) {
         this.adjustments.add(adjustment);
-        this.production += adjustment;
+//        this.production += adjustment;
     }
 
     public double getProduction() {
-        return this.production;
+        return this.adjustments.stream().mapToDouble(Double::valueOf).sum();
     }
 }
